251e81f5248533493ab6aa29f3db22b4a75e17e9
lucaschavesalb <lucaschavesalbuquerque06@gmail.com>
2025-11-29 22:13:29 -0300

    Adicionado comentarios para minha parte

--- PIF/fleet.c
+++ PIF/fleet.c
@@ -5,12 +5,11 @@
 
 void inicializarFrota(Frota *f) {
     f->navios = NULL;
-    f->quantidade = 0;
+    f->quantidade = 0; // Começo com a frota vazia, sem navio e com quantidade zero. Dps conforme eu for adicionando navios o vetor é criado com realloc
 }
 
 void adicionarNavioNaFrota(Frota *f, const char *nome, int tamanho) {
-    // [cite: 150] Uso explícito de realloc
-    Navio *novo = (Navio *) realloc(f->navios, (f->quantidade + 1) * sizeof(Navio));
+    Navio *novo = (Navio *) realloc(f->navios, (f->quantidade + 1) * sizeof(Navio)); // Breve resumo pra lembrar: dá espaço pra mais um navio, mantendo os antigos.
     Navio *n;
 
     if (novo == NULL) {
@@ -18,19 +17,19 @@ void adicionarNavioNaFrota(Frota *f, const char *nome, int tamanho) {
         exit(1);
     }
 
-    f->navios = novo;
+    f->navios = novo; // o novo que vai ser preenchido ligando com a linha 12.
     n = &f->navios[f->quantidade];
-
-    strncpy(n->nome, nome, sizeof(n->nome) - 1);
+                        // sizeof(n->nome) - 1 o maximo vai ser decido aqui.
+    strncpy(n->nome, nome, sizeof(n->nome) - 1); // strncpy: copiar uma sequência de caracteres de uma origem para um destino, com um limite máximo de caracteres a serem copiados, uma medida de segurança.
     n->nome[sizeof(n->nome) - 1] = '\0';
     n->tamanho = tamanho;
     n->acertos = 0;
 
-    f->quantidade++;
+    f->quantidade++; // dalhe navio.
 }
 
 void criarFrotaPadrao(Frota *f) {
-    // [cite: 13-16] Configuração padrão da frota
+     // A frota minima pedida de acordo com o Projeto.
     adicionarNavioNaFrota(f, "Porta-avioes", 5);
     adicionarNavioNaFrota(f, "Encouracado", 4);
     adicionarNavioNaFrota(f, "Cruzador", 3);
@@ -39,17 +38,18 @@ void criarFrotaPadrao(Frota *f) {
     adicionarNavioNaFrota(f, "Destroyer", 2);
 }
 
-void liberarFrota(Frota *f) {
+void liberarFrota(Frota *f) {  // aqui a gnt vai liberar a memoria pedida lá encima.
     if (f->navios != NULL) {
         free(f->navios);
         f->navios = NULL;
     }
-    f->quantidade = 0;
+    f->quantidade = 0; // zeramos o ponteiro e a quantidade pra evitar uso indevido
 }
 
 int frotaAfundou(Frota *f) {
     int i;
-    for (i = 0; i < f->quantidade; i++) {
+    for (i = 0; i < f->quantidade; i++) { // aqui percorre todos os navios da frota e verifica se todos já foram totalmente acertados
+                            // Se encontra um navio não afundado
         if (f->navios[i].acertos < f->navios[i].tamanho) return 0;
     }
     return 1;
--- PIF/fleet.h
+++ PIF/fleet.h
@@ -4,11 +4,11 @@
 typedef struct {
     char nome[20];
     int tamanho;
-    int acertos;
+    int acertos; // lembrar que acertos==tamanho, ja era papai  (Exemplo: se eu tenho um navio com 5 de tamanho e ele foi atigindo 5 vezes ja era)
 } Navio;
 
 typedef struct {
-    Navio *navios;    /* malloc / realloc */
+    Navio *navios;   // malloc e realloc //
     int quantidade;
 } Frota;
 
